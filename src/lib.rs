314c8b317bdeadc8d2412ece52dc5a62312ee648
Vit <vit@conrlab.com>
2024-09-23 21:04:31 +0700

    MacOS

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,12 @@ pub type Result<T> = std::result::Result<T, error::Error>;
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct MachineId(Uuid);
 
+impl std::fmt::Display for MachineId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::UpperHex::fmt(&self.0, f)
+    }
+}
+
 impl AsRef<Uuid> for MachineId {
     #[inline]
     fn as_ref(&self) -> &Uuid {
@@ -53,6 +59,49 @@ impl MachineId {
 
         Ok(Self(machine_uuid))
     }
+
+    #[cfg(target_os = "macos")]
+    pub fn new() -> Result<Self> {
+        use apple_sys::IOKit as io;
+        use core_foundation::{
+            base::TCFType,
+            string::{CFString, CFStringRef},
+        };
+
+        struct ObjectReleaser(u32);
+        impl Drop for ObjectReleaser {
+            fn drop(&mut self) {
+                unsafe { io::IOObjectRelease(self.0) };
+            }
+        }
+
+        let uuid_str = unsafe {
+            let root = io::IORegistryEntryFromPath(
+                io::kIOMasterPortDefault,
+                "IOService:/\0".as_ptr() as _,
+            );
+
+            if root == io::MACH_PORT_NULL {
+                return Err(std::io::Error::last_os_error().into());
+            }
+
+            let root = ObjectReleaser(root);
+            let key = CFString::from_static_string("IOPlatformUUID");
+            let uuid_cref: CFStringRef = io::IORegistryEntryCreateCFProperty(
+                root.0,
+                key.as_CFTypeRef() as _,
+                io::kCFAllocatorDefault,
+                0,
+            ) as _;
+
+            if uuid_cref.is_null() {
+                return Err(std::io::Error::last_os_error().into());
+            }
+            CFString::wrap_under_create_rule(uuid_cref).to_string()
+        };
+
+        Ok(Self(uuid::Uuid::parse_str(&uuid_str)?))
+    }
 }
 
 #[cfg(test)]
@@ -63,6 +112,8 @@ mod tests {
     fn test_get_machine_id() {
         let first = MachineId::new().unwrap();
         let second = MachineId::new().unwrap();
+
+        println!("Machine id: {first}");
 
         assert_eq!(first, second);
     }
